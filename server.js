86e6e233a65cf6486fa43eefe596df4f76b2ac49
ankitrajsingh <ankitpulkit.raj@gmail.com>
2016-09-18 16:38:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-   'articleOne' : {
+   'article-one' : {
 	title: 'article One  | Ankit',
 	heading: 'Article One',
 	date: 'Sep 5,2016',
@@ -23,7 +23,7 @@ var articles = {
               This is the content for my first article. This is the content for my first article. This is the content for my first article.
               </p>`
         },
-   'articleTwo' : {
+   'article-two' : {
        title: 'article Two  | Ankit',
 	heading: 'Article Two',
 	date: 'Sep 15,2016',
@@ -39,7 +39,7 @@ var articles = {
               This is the content for my first article. This is the content for my first article. 
               This is the content for my first article. This is the content for my first article. This is the content for my first article.
               </p>`  },
-   'articleThree' : {
+   'article-three' : {
         title: 'article three  | Ankit',
         heading: 'Article Three',
         date: 'Sep 25,2016',
