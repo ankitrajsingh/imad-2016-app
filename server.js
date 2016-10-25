d9a89cfff7a31781c6770c7d81105da83894bffe
ankitrajsingh <ankitpulkit.raj@gmail.com>
2016-10-25 17:56:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,11 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/test-db', function(req,res){
+    // make a select request
+    //return response with result
+});
 var counter = 0;
 app.get('/counter',function(req,res){
   counter = counter + 1;
